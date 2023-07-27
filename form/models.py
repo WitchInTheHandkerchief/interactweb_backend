b6269a4155a3f162cc34e9423c96d1477cc8a6b9
ilimk <ilimkanu@gmail.com>
2023-07-27 19:03:20 +0600

    feat: add models

--- form/models.py
+++ form/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Form(models.Model):
+    name = models.CharField(_("Имя"), max_length=128)
+    date_of_birth = models.DateField(_("Дата рождения"))
+    phone_number = models.CharField(_("Номер телефона"), max_length=64)
+
+    class Meta:
+        verbose_name = _("form")
+        verbose_name_plural = _("forms")
+
+    def __str__(self):
+        return f'{self.name}'
+
--- news/models.py
+++ news/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(_("Заголовок"), max_length=512)
+    date = models.DateField(_("Дата"), auto_now_add=True)
+    preview_url = models.URLField(_("Картинка"), null=True, blank=True, max_length=256)
+    content = models.TextField(_("Текст"))
+
+    class Meta:
+        verbose_name = _("news")
+        verbose_name_plural = _("news")
+
+    def __str__(self):
+        return f'{self.title}, {self.date}'

5a460df3332132fbe2162bebfc1badec33078313
ilimk <ilimkanu@gmail.com>
2023-08-09 07:21:32 +0600

    feat: add endpoint for 3 last news

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from news import views
 
 urlpatterns = [
-    path("list_news/", views.NewsAPIView.as_view())
+    path("list_news/", views.NewsAPIView.as_view()),
+    path("list_three_news/", views.ThreeLastNewsAPIView.as_view())
 ]
--- news/views.py
+++ news/views.py
@@ -9,3 +9,9 @@ class NewsAPIView(generics.ListAPIView):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
     permission_classes = [AllowAny]
+
+
+class ThreeLastNewsAPIView(generics.ListAPIView):
+    queryset = News.objects.order_by("-date", "title")[:3]
+    serializer_class = NewsSerializer
+    permission_classes = [AllowAny]
